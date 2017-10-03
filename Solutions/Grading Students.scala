cb196dec6ed2acdbfe79e09ceeaa3277facc4582
Somdip Dey <somdip007@hotmail.com>
2017-10-03 20:27:28 +0100

    Create Grading Students.scala

---
+++ Solutions/Grading Students.scala
@@ -0,0 +1,24 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+        var grades = new Array[Int](n);
+        for(grades_i <- 0 to n-1) {
+           grades(grades_i) = sc.nextInt();
+        }
+        
+        for(i <- Range(0,grades.length))
+        {
+            println(evaluateGrade(grades(i)))
+        }
+    }
+    
+    def evaluateGrade(n: Int): Int = (n % 5 > 2, n < 38) match {
+        case (true, true) => n
+        case (false, true) => n
+        case (true, false) => n + (5 - (n % 5))
+        case (false, false) => n
+        case(_,_) => 0
+    }
+}
