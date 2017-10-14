ce8d9f891d1b8848d2edf89425d5a31cc332d66f
Somdip Dey <somdip007@hotmail.com>
2017-10-14 01:04:53 +0100

    Update Day 10: Binary Numbers.scala

--- Solutions/Day 10: Binary Numbers.scala
+++ Solutions/Day 10: Binary Numbers.scala
@@ -3,19 +3,9 @@ object Solution {
     def main(args: Array[String]) {
         val sc = new java.util.Scanner (System.in);
         var n = sc.nextInt();
-        var maxResult = 0
         
         var temp = n.toBinaryString.toInt
-        var binaryList = temp.toString.toArray
-        var consecutiveOnes = 0
-        
-        for(i <- 0 to binaryList.size - 1)
-        {
-                if(binaryList(i).toString == "1") consecutiveOnes = consecutiveOnes + 1
-                else consecutiveOnes = 0
-            if(maxResult < consecutiveOnes) maxResult = consecutiveOnes
-        }
-        
-        println(maxResult)
+        var listOfOnes = temp.toString.split("0").toList
+        println(listOfOnes.max.size)
     }
 }
